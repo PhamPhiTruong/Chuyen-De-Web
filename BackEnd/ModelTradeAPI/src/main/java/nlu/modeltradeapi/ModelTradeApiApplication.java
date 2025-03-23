65effb037145a2590f65e3e7c3c5272cd3b39f43
Truong <146102566+PhamPhiTruong@users.noreply.github.com>
2025-03-23 21:50:51 +0700

    First time add file

---
+++ BackEnd/ModelTradeAPI/src/main/java/nlu/modeltradeapi/ModelTradeApiApplication.java
@@ -0,0 +1,13 @@
+package nlu.modeltradeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelTradeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModelTradeApiApplication.class, args);
+    }
+
+}
---
+++ FrontEnd/model-trade-app/app/users/new/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <p>Sign The Contract To Join</p>
+      <Link href='/'>Test</Link>
+    </div>
+  )
+}
+
+export default RegisterPage
