9fd5e8ff8431781f56433c96bb2955a0efd96d7c
Truong <146102566+PhamPhiTruong@users.noreply.github.com>
2025-05-05 22:51:02 +0700

    Xong CRUD co ban cho USER

--- BackEnd/ModelTradeAPI/src/main/java/nlu/modeltradeapi/services/template/IUserService.java
+++ BackEnd/ModelTradeAPI/src/main/java/nlu/modeltradeapi/services/template/IUserService.java
@@ -1,9 +1,16 @@
 package nlu.modeltradeapi.services.template;
 
 import nlu.modeltradeapi.dtos.requestdto.user.UserRegisterRequestDTO;
+import nlu.modeltradeapi.dtos.requestdto.user.UserUpdateRequestDTO;
 import nlu.modeltradeapi.dtos.responsedto.MessageResponseDTO;
 import nlu.modeltradeapi.entities.User;
 
+import java.util.List;
+
 public interface IUserService {
     public User registerUser(UserRegisterRequestDTO registerRequest);
+    public List<User> getUsers();
+    public User getUserById(String userId);
+    public User updateUser(String userId, UserUpdateRequestDTO updateRequest);
+    public void deleteUser(String userId);
 }
