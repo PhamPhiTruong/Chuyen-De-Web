8330baa90d6d7e387397e5149557ff5ceebec8e6
TranSon <hoangson145juzk@gmail.com>
2025-05-16 16:10:25 +0700

    xu ly jwt

--- BackEnd/ModelTradeAPI/src/main/java/nlu/modeltradeapi/dtos/responsedto/IntrospecResponseonseDTO.java
+++ BackEnd/ModelTradeAPI/src/main/java/nlu/modeltradeapi/dtos/responsedto/IntrospecResponseonseDTO.java
@@ -2,14 +2,12 @@ package nlu.modeltradeapi.dtos.responsedto;
 
 import lombok.*;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @FieldDefaults (level = AccessLevel.PRIVATE)
-public class AuthenticationResponseonse {
-    String token;
-    boolean authenticated;
+public class IntrospecResponseonseDTO {
+    boolean valid;
 }
