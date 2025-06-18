c6756cbdf5892a435ca5c32d85f0300752eac9e7
Truong <146102566+PhamPhiTruong@users.noreply.github.com>
2025-06-18 11:18:02 +0700

    Sua lai giao dien mot it

--- FrontEnd/model-trade-app/app/components/inventory/UploadProduct.tsx
+++ FrontEnd/model-trade-app/app/components/inventory/UploadProduct.tsx
@@ -135,14 +135,14 @@ const SellProduct: React.FC<SellProductProps> = ({ token }) => {
           <div className="md:w-7/10">
             <form onSubmit={handleSubmit} className="grid gap-6  px-10 pb-10">
               <p className="font-bold text-2xl">Thông tin chi tiết</p>
-              <div className="flex items-center gap-4">
+              {/* <div className="flex items-center gap-4">
                 <p>Series sản phẩm</p>
                 <div className="bg-yellow-300 p-2 w-fit rounded">
                   <p>Mô hình</p>
                 </div>
                 <FaPlus className="bg-gray-200 p-1 h-fit w-7 hover:bg-gray-300" />
-              </div>
-              <div className="md:flex items-center md:gap-4">
+              </div> */}
+              {/* <div className="md:flex items-center md:gap-4">
                 <div>
                   <p className="my-2">Tình trạng</p>
                   <RadioButton
@@ -162,111 +162,131 @@ const SellProduct: React.FC<SellProductProps> = ({ token }) => {
                     onChange={handleRadioChange("purpose")}
                   />
                 </div>
-              </div>
+              </div> */}
               <div className="items-center gap-4 grid">
-                <textarea
-                  className="h-fit w-full border-1 px-2 py-2 focus:border-4 rounded-lg"
-                  placeholder="Tên sản phẩm"
-                  name="productName"
-                  value={modelName}
-                  onChange={(e) => setModelName(e.target.value)}
-                ></textarea>
-                <input
-                  type="number"
-                  className="h-fit w-full border-1 px-2 py-2 focus:border-4 rounded-lg"
-                  placeholder="Giá sản phẩm (>= 10.000 VND)"
-                  name="price"
-                  value={price}
-                  min={10000}
-                  onChange={(e) => setPrice(Number(e.target.value))}
-                />
-                <textarea
-                  className="h-fit w-full border-1 px-2 py-2 focus:border-4 rounded-lg"
-                  placeholder="Mô tả sản phẩm"
-                  name="description"
-                  value={description}
-                  onChange={(e) => setDescription(e.target.value)}
-                ></textarea>
+                <div className="flex items-center gap-4">
+                  <span className="whitespace-nowrap">Nhập tên mô hình</span>
+                  <textarea
+                    className="h-fit w-full border px-2 py-2 focus:border-4 rounded-lg"
+                    placeholder="Tên sản phẩm"
+                    name="productName"
+                    value={modelName}
+                    onChange={(e) => setModelName(e.target.value)}
+                  ></textarea>
+                </div>
+                <div className="flex items-center gap-4">
+                  <span className="whitespace-nowrap">Nhập giá mô hình</span>
+                  <input
+                    type="number"
+                    className="h-fit w-full border-1 px-2 py-2 focus:border-4 rounded-lg"
+                    placeholder="Giá sản phẩm (>= 10.000 VND)"
+                    name="price"
+                    value={price}
+                    min={10000}
+                    onChange={(e) => setPrice(Number(e.target.value))}
+                  />
+                </div>
+                <div className="flex items-center gap-4">
+                  <span className="whitespace-nowrap">Nhập mô tả cho mô hình</span>
+                  <textarea
+                    className="h-fit w-full border-1 px-2 py-2 focus:border-4 rounded-lg"
+                    placeholder="Mô tả sản phẩm"
+                    name="description"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                  ></textarea>
+                </div>
+                <div className="flex items-center gap-4">
+                  <span className="whitespace-nowrap">Nhập số lượng</span>
+                  <input
+                    type="number"
+                    className="h-fit w-full border-1 px-2 py-2 focus:border-4 rounded-lg"
+                    placeholder="Số lượng >= 10"
+                    name="quantity"
+                    value={quantity}
+                    min={10}
+                    onChange={(e) => setPrice(Number(e.target.value))}
+                  />
+                </div>
 
-                <select
-                  className="h-fit w-full border-1 px-2 py-2 focus:border-4 rounded-lg"
-                  defaultValue=""
-                  name="address"
-                >
-                  {/* <option disabled value="" className="text-gray-400">
-                    Địa chỉ
-                  </option>
-                  <option value="Thu Duc">Thủ Đức</option>
-                  <option value="Da Nang">Đà Nẵng</option>
-                  <option value="Da Nang">Hà Nội</option> */}
-                  <option disabled value="" className="text-gray-400">
-                    Địa chỉ
-                  </option>
-                  <option value="TP Hồ Chí Minh">Hồ Chí Minh</option>
-                  <option value="An Giang">An Giang</option>
-                  <option value="Bà Rịa - Vũng Tàu">Bà Rịa - Vũng Tàu</option>
-                  <option value="Bắc Giang">Bắc Giang</option>
-                  <option value="Bắc Kạn">Bắc Kạn</option>
-                  <option value="Bạc Liêu">Bạc Liêu</option>
-                  <option value="Bắc Ninh">Bắc Ninh</option>
-                  <option value="Bến Tre">Bến Tre</option>
-                  <option value="Bình Định">Bình Định</option>
-                  <option value="Bình Dương">Bình Dương</option>
-                  <option value="Bình Phước">Bình Phước</option>
-                  <option value="Bình Thuận">Bình Thuận</option>
-                  <option value="Cà Mau">Cà Mau</option>
-                  <option value="Cần Thơ">Cần Thơ</option>
-                  <option value="Cao Bằng">Cao Bằng</option>
-                  <option value="Đà Nẵng">Đà Nẵng</option>
-                  <option value="Đắk Lắk">Đắk Lắk</option>
-                  <option value="Đắk Nông">Đắk Nông</option>
-                  <option value="Điện Biên">Điện Biên</option>
-                  <option value="Đồng Nai">Đồng Nai</option>
-                  <option value="Đồng Tháp">Đồng Tháp</option>
-                  <option value="Gia Lai">Gia Lai</option>
-                  <option value="Hà Giang">Hà Giang</option>
-                  <option value="Hà Nam">Hà Nam</option>
-                  <option value="Hà Nội">Hà Nội</option>
-                  <option value="Hà Tĩnh">Hà Tĩnh</option>
-                  <option value="Hải Dương">Hải Dương</option>
-                  <option value="Hải Phòng">Hải Phòng</option>
-                  <option value="Hậu Giang">Hậu Giang</option>
-                  <option value="Hòa Bình">Hòa Bình</option>
-                  <option value="Hưng Yên">Hưng Yên</option>
-                  <option value="Khánh Hòa">Khánh Hòa</option>
-                  <option value="Kiên Giang">Kiên Giang</option>
-                  <option value="Kon Tum">Kon Tum</option>
-                  <option value="Lai Châu">Lai Châu</option>
-                  <option value="Lâm Đồng">Lâm Đồng</option>
-                  <option value="Lạng Sơn">Lạng Sơn</option>
-                  <option value="Lào Cai">Lào Cai</option>
-                  <option value="Long An">Long An</option>
-                  <option value="Nam Định">Nam Định</option>
-                  <option value="Nghệ An">Nghệ An</option>
-                  <option value="Ninh Bình">Ninh Bình</option>
-                  <option value="Ninh Thuận">Ninh Thuận</option>
-                  <option value="Phú Thọ">Phú Thọ</option>
-                  <option value="Phú Yên">Phú Yên</option>
-                  <option value="Quảng Bình">Quảng Bình</option>
-                  <option value="Quảng Nam">Quảng Nam</option>
-                  <option value="Quảng Ngãi">Quảng Ngãi</option>
-                  <option value="Quảng Ninh">Quảng Ninh</option>
-                  <option value="Quảng Trị">Quảng Trị</option>
-                  <option value="Sóc Trăng">Sóc Trăng</option>
-                  <option value="Sơn La">Sơn La</option>
-                  <option value="Tây Ninh">Tây Ninh</option>
-                  <option value="Thái Bình">Thái Bình</option>
-                  <option value="Thái Nguyên">Thái Nguyên</option>
-                  <option value="Thanh Hóa">Thanh Hóa</option>
-                  <option value="Thừa Thiên Huế">Thừa Thiên Huế</option>
-                  <option value="Tiền Giang">Tiền Giang</option>
-                  {/* <option value="TP Hồ Chí Minh">TP Hồ Chí Minh</option> */}
-                  <option value="Trà Vinh">Trà Vinh</option>
-                  <option value="Tuyên Quang">Tuyên Quang</option>
-                  <option value="Vĩnh Long">Vĩnh Long</option>
-                  <option value="Vĩnh Phúc">Vĩnh Phúc</option>
-                  <option value="Yên Bái">Yên Bái</option>
-                </select>
+                <div className="flex items-center gap-4">
+                  <span className="whitespace-nowrap">Chọn địa chỉ</span>
+                  <select
+                    className="h-fit w-full border-1 px-2 py-2 focus:border-4 rounded-lg"
+                    defaultValue=""
+                    name="address"
+                  >
+                    <option disabled value="" className="text-gray-400">
+                      Địa chỉ
+                    </option>
+                    <option value="TP Hồ Chí Minh">Hồ Chí Minh</option>
+                    <option value="An Giang">An Giang</option>
+                    <option value="Bà Rịa - Vũng Tàu">Bà Rịa - Vũng Tàu</option>
+                    <option value="Bắc Giang">Bắc Giang</option>
+                    <option value="Bắc Kạn">Bắc Kạn</option>
+                    <option value="Bạc Liêu">Bạc Liêu</option>
+                    <option value="Bắc Ninh">Bắc Ninh</option>
+                    <option value="Bến Tre">Bến Tre</option>
+                    <option value="Bình Định">Bình Định</option>
+                    <option value="Bình Dương">Bình Dương</option>
+                    <option value="Bình Phước">Bình Phước</option>
+                    <option value="Bình Thuận">Bình Thuận</option>
+                    <option value="Cà Mau">Cà Mau</option>
+                    <option value="Cần Thơ">Cần Thơ</option>
+                    <option value="Cao Bằng">Cao Bằng</option>
+                    <option value="Đà Nẵng">Đà Nẵng</option>
+                    <option value="Đắk Lắk">Đắk Lắk</option>
+                    <option value="Đắk Nông">Đắk Nông</option>
+                    <option value="Điện Biên">Điện Biên</option>
+                    <option value="Đồng Nai">Đồng Nai</option>
+                    <option value="Đồng Tháp">Đồng Tháp</option>
+                    <option value="Gia Lai">Gia Lai</option>
+                    <option value="Hà Giang">Hà Giang</option>
+                    <option value="Hà Nam">Hà Nam</option>
+                    <option value="Hà Nội">Hà Nội</option>
+                    <option value="Hà Tĩnh">Hà Tĩnh</option>
+                    <option value="Hải Dương">Hải Dương</option>
+                    <option value="Hải Phòng">Hải Phòng</option>
+                    <option value="Hậu Giang">Hậu Giang</option>
+                    <option value="Hòa Bình">Hòa Bình</option>
+                    <option value="Hưng Yên">Hưng Yên</option>
+                    <option value="Khánh Hòa">Khánh Hòa</option>
+                    <option value="Kiên Giang">Kiên Giang</option>
+                    <option value="Kon Tum">Kon Tum</option>
+                    <option value="Lai Châu">Lai Châu</option>
+                    <option value="Lâm Đồng">Lâm Đồng</option>
+                    <option value="Lạng Sơn">Lạng Sơn</option>
+                    <option value="Lào Cai">Lào Cai</option>
+                    <option value="Long An">Long An</option>
+                    <option value="Nam Định">Nam Định</option>
+                    <option value="Nghệ An">Nghệ An</option>
+                    <option value="Ninh Bình">Ninh Bình</option>
+                    <option value="Ninh Thuận">Ninh Thuận</option>
+                    <option value="Phú Thọ">Phú Thọ</option>
+                    <option value="Phú Yên">Phú Yên</option>
+                    <option value="Quảng Bình">Quảng Bình</option>
+                    <option value="Quảng Nam">Quảng Nam</option>
+                    <option value="Quảng Ngãi">Quảng Ngãi</option>
+                    <option value="Quảng Ninh">Quảng Ninh</option>
+                    <option value="Quảng Trị">Quảng Trị</option>
+                    <option value="Sóc Trăng">Sóc Trăng</option>
+                    <option value="Sơn La">Sơn La</option>
+                    <option value="Tây Ninh">Tây Ninh</option>
+                    <option value="Thái Bình">Thái Bình</option>
+                    <option value="Thái Nguyên">Thái Nguyên</option>
+                    <option value="Thanh Hóa">Thanh Hóa</option>
+                    <option value="Thừa Thiên Huế">Thừa Thiên Huế</option>
+                    <option value="Tiền Giang">Tiền Giang</option>
+                    {/* <option value="TP Hồ Chí Minh">TP Hồ Chí Minh</option> */}
+                    <option value="Trà Vinh">Trà Vinh</option>
+                    <option value="Tuyên Quang">Tuyên Quang</option>
+                    <option value="Vĩnh Long">Vĩnh Long</option>
+                    <option value="Vĩnh Phúc">Vĩnh Phúc</option>
+                    <option value="Yên Bái">Yên Bái</option>
+                  </select>
+                </div>
+
+
               </div>
               <button
                 type="submit"
